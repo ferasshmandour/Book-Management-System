246c37af312a2353f47681b4a91f5c3d1b1401a1
feras <ferasshamandour2@gmail.com>
2026-01-02 17:29:15 +0300

    Fix style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
 
     @include('partials.header')
 
-    <div class="container-fluid flex-grow-1">
+    <div class="container-fluid">
         <div class="row h-100">
 
             <aside class="col-md-3 col-lg-2 bg-dark text-light p-3">
@@ -35,19 +35,7 @@
 
             <div class="col-md-9 col-lg-10 p-4">
                 <main class="bg-white rounded-4 shadow-sm p-4">
-                    @if (session('success'))
-                        <div class="alert alert-success alert-dismissible fade show" role="alert">
-                            {{ session('success') }}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                        </div>
-                    @endif
-
-                    @if (session('error'))
-                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                            {{ session('error') }}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                        </div>
-                    @endif
+                    <x-alert />
                     @yield('content')
                 </main>
             </div>
